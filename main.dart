caf19dcd2fb7f160e9e1cbb8d8d48bec588f716e
Ecky819 <marcoeggert73@gmail.com>
2025-06-25 11:53:10 +0200

    3 Klassen angelegt

---
+++ main.dart
@@ -0,0 +1,73 @@
+class user {
+  int user_id;
+  String surname;
+  String name;
+  String email;
+  String password;
+  String land;
+
+  user(
+    this.user_id,
+    this.surname,
+    this.name,
+    this.email,
+    this.password,
+    this.land,
+  );
+}
+
+class victim {
+  int victim_id;
+  String surname;
+  String name;
+  DateTime birth;
+  String birthplace;
+  DateTime death;
+  String deathplace;
+  String nationality;
+  String religion;
+  String occupation;
+  bool death_certificate;
+  String c_camp;
+  String fate;
+
+  victim(
+    this.victim_id,
+    this.surname,
+    this.name,
+    this.birth,
+    this.birthplace,
+    this.death,
+    this.deathplace,
+    this.nationality,
+    this.religion,
+    this.occupation,
+    this.death_certificate,
+    this.c_camp,
+    this.fate,
+  );
+}
+
+class concentration_camp {
+  int camp_id;
+  String name;
+  String location;
+  String country;
+  String description;
+  DateTime date_opened;
+  DateTime liberation_date;
+  String type;
+  String commander;
+
+  concentration_camp(
+    this.camp_id,
+    this.name,
+    this.location,
+    this.country,
+    this.description,
+    this.date_opened,
+    this.liberation_date,
+    this.type,
+    this.commander,
+  );
+}
